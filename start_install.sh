6ee063fe2aa09b579451ef85cbfe054e34ce5c11
Dmytro Kostiuchenko <138630083+proxierdima@users.noreply.github.com>
2026-04-03 09:04:37 +0300

    Update start_install.sh

--- start_install.sh
+++ start_install.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo $ROOT_DIR
 BACKEND_DIR="$ROOT_DIR/backend"
 FRONTEND_DIR="$ROOT_DIR/frontend"
 BACKEND_PORT="${BACKEND_PORT:-8000}"
